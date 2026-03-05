249bdb6dcfe638d3823b539c0ac67fe9376c6f78
Santiago Toledo <santiagotoledosilvera@gmail.com>
2026-03-04 22:42:40 -0500

    feature: Se completa la tercera función.

--- usuario_b.py
+++ usuario_b.py
@@ -39,3 +39,10 @@ def verificar_primo():
 #_________________________________________________________
 
 def factorial():
+    n = int(input("Ingrese un número: "))
+    resultado = 1
+
+    for i in range(1, n + 1):
+        resultado = resultado * i
+
+    print("El factorial es:", resultado)
